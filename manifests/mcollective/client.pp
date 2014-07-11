0968e8d1dc14b420812c7867d5c5d2708cfe467c
Rob Nelson <rnelson0@gmail.com>
2014-07-11 18:18:48 +0000

    Add profile::mcollective::client class

---
+++ manifests/mcollective/client.pp
@@ -0,0 +1,36 @@
+# == Class: profile::mcollective::client
+#
+# Profile for an mcollective client
+#
+# === Parameters
+#
+# None
+#
+# === Variables
+#
+# None
+#
+# === Examples
+#
+# include profile::mcollective::client
+#
+# === Authors
+#
+# Rob Nelson <rnelson0@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Rob Nelson
+#
+class profile::mcollective::client {
+  class { '::mcollective':
+    client             => true,
+    middleware_hosts   => [ 'puppet.nelson.va' ],
+    middleware_ssl     => true,
+    securityprovider   => 'ssl',
+    ssl_client_certs   => 'puppet:///modules/site_mcollective/client_certs',
+    ssl_ca_cert        => 'puppet:///modules/site_mcollective/certs/ca.pem',
+    ssl_server_public  => 'puppet:///modules/site_mcollective/certs/puppet.nelson.va.pem',
+    ssl_server_private => 'puppet:///modules/site_mcollective/private_keys/puppet.nelson.va.pem',
+  }
+}
