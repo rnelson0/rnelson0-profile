476093391955dd1ebb504acbad32d7d998fdf192
Rob Nelson <rnelson0@gmail.com>
2014-09-25 13:18:21 +0000

    Add new profile ::profile::mcollective::server

---
+++ manifests/mcollective/server.pp
@@ -0,0 +1,36 @@
+# == Class: profile::mcollective::server
+#
+# Role for an mcollective server node.
+# Server in this case indicates a node that is managed by mcollective.
+#
+# === Parameters
+#
+# None
+#
+# === Variables
+#
+# None
+#
+# === Examples
+#
+# include profile::mcollective::server
+#
+# === Authors
+#
+# Rob Nelson <rnelson0@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Rob Nelson
+#
+class profile::mcollective::server {
+  class { '::mcollective':
+    middleware_hosts   => [ 'puppet.nelson.va' ],
+    middleware_ssl     => true,
+    securityprovider   => 'ssl',
+    ssl_client_certs   => 'puppet:///modules/site_mcollective/client_certs',
+    ssl_ca_cert        => 'puppet:///modules/site_mcollective/certs/ca.pem',
+    ssl_server_public  => 'puppet:///modules/site_mcollective/certs/puppet.nelson.va.pem',
+    ssl_server_private => 'puppet:///modules/site_mcollective/private_keys/puppet.nelson.va.pem',
+  }
+}
