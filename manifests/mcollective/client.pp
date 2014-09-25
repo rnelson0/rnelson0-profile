df211abb1518f5c85a2b7744b5c5aab12742261c
Rob Nelson <rnelson0@gmail.com>
2014-09-25 13:21:01 +0000

    Update ::profile::mcollective::client to streamline requirements to a single profile.

--- manifests/mcollective/client.pp
+++ manifests/mcollective/client.pp
@@ -1,6 +1,8 @@
 # == Class: profile::mcollective::client
 #
-# Profile for an mcollective client
+# Profile for an mcollective client node
+# Client in context indicates a node where 'mco' can be run.
+# It must be able to communicate with the middleware_host.
 #
 # === Parameters
 #
@@ -32,5 +34,19 @@ class profile::mcollective::client {
     ssl_ca_cert        => 'puppet:///modules/site_mcollective/certs/ca.pem',
     ssl_server_public  => 'puppet:///modules/site_mcollective/certs/puppet.nelson.va.pem',
     ssl_server_private => 'puppet:///modules/site_mcollective/private_keys/puppet.nelson.va.pem',
+  }
+
+  $plugins = [ 'service', 'package', 'puppet']
+  mcollective::plugin { $plugins:
+    package => true,
+  }
+
+  user { 'root':
+    ensure => present,
+  } ->
+  mcollective::user { 'root':
+    homedir     => '/root',
+    certificate => 'puppet:///modules/site_mcollective/client_certs/root.pem',
+    private_key => 'puppet:///modules/site_mcollective/private_keys/root.pem',
   }
 }
