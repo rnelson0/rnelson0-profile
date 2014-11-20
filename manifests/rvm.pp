a10d7272cfd594c2f26b100b343a6300e04983ba
Rob Nelson <rnelson0@gmail.com>
2014-11-20 03:37:08 +0000

    Add some gems to profile::rvm

--- manifests/rvm.pp
+++ manifests/rvm.pp
@@ -1,9 +1,17 @@
 class profile::rvm {
-  include rvm
+  include '::rvm'
 
-  rvm_system_ruby {
-    'ruby-2.0':
-      ensure      => present,
-      default_use => true,
+  $ruby_version = 'ruby-1.9.3-p488'
+  rvm_system_ruby { $ruby_version:
+    ensure      => present,
+    default_use => true,
+  }
+
+  $rvm_gems = ['rspec-puppet', 'puppet', 'fpm', 'puppet-lint']
+  rvm_gem { $rvm_gems:
+    name         => $title,
+    ruby_version => $ruby_version,
+    ensure       => latest,
+    require      => Rvm_system_ruby[$ruby_version],
   }
 }
