929d2de3adeccff5da9bce84ecd2dff3e25ff56c
Rob Nelson <rnelson0@gmail.com>
2014-11-20 05:16:16 +0000

    Add puppetlabs_spec_helper to profile::rvm, reformat $rvm_gems array.

--- manifests/rvm.pp
+++ manifests/rvm.pp
@@ -7,7 +7,13 @@ class profile::rvm {
     default_use => true,
   }
 
-  $rvm_gems = ['rspec-puppet', 'puppet', 'fpm', 'puppet-lint']
+  $rvm_gems = [
+    'rspec-puppet',
+    'puppet',
+    'fpm',
+    'puppet-lint',
+    'puppetlabs_spec_helper',
+  ]
   rvm_gem { $rvm_gems:
     ruby_version => $ruby_version,
     ensure       => latest,
