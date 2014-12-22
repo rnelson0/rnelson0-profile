7952fd9129e6ddcdcdaca196a56050902f6ffb97
Rob Nelson <rn7284@att.com>
2014-12-22 15:42:51 +0000

    Create dependency chain between ssh-related files.

--- manifests/sshgw.pp
+++ manifests/sshgw.pp
@@ -17,11 +17,15 @@ class profile::sshgw {
     create_resources('host', $sshvpn_hosts)
   }
 
+  # SSH directory and keys
+  file { '/root/.ssh':
+    ensure   => 'directory',
+    mode     => '0700',
+  } -> 
   file { '/root/.ssh/id_rsa':
     ensure => file,
     source => "puppet:///modules/home_config/${puppet_role}/id_rsa",
-  }
-
+  } ->
   file { '/root/.ssh/id-rsa.pub':
     ensure => file,
     source => "puppet:///modules/home_config/${puppet_role}/id_rsa.pub",
