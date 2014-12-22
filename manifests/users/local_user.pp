bca5a51a51574a6835cec159ed6ff3290eb8f997
Rob Nelson <rn7284@att.com>
2014-12-22 03:43:08 +0000

    Add password_max_age to profile::users::local_user defined type. Clean up formatting.

--- manifests/users/local_user.pp
+++ manifests/users/local_user.pp
@@ -13,11 +13,12 @@
 # === Examples
 #
 #  ::profile::users::local_user { 'username':
-#    id    => 'username',
-#    state => 'present',
-#    comment => 'Real Name",
-#    groups  => ['group1', 'group2'],
-#    password => 'encryptedstring',
+#    id               => 'username',
+#    state            => 'present',
+#    comment          => 'Real Name",
+#    groups           => ['group1', 'group2'],
+#    password         => 'encryptedstring',
+#    password_max_age => 90,
 #  }
 #
 # === Authors
@@ -28,26 +29,33 @@
 #
 # Copyright 2014 Rob Nelson
 #
-define profile::users::local_user ( $state, $id, $comment, $groups, $password) {
-    user { $id:
-            ensure           => $state,
-            shell            => '/bin/bash',
-            home             => "/home/${id}",
-            comment          => $comment,
-            managehome       => true,
-            groups           => $groups,
-            password_max_age => '90',
-    }
+define profile::users::local_user (
+  $state,
+  $id,
+  $comment,
+  $groups,
+  $password,
+  $password_max_age = 90,
+) {
+  user { $id:
+    ensure           => $state,
+    shell            => '/bin/bash',
+    home             => "/home/${id}",
+    comment          => $comment,
+    managehome       => true,
+    groups           => $groups,
+    password_max_age => $password_max_age,
+  }
 
-    case $::osfamily {
-            RedHat: {$action = "/bin/sed -i -e 's/${id}:!!:/${id}:${password}:/g' /etc/shadow; chage -d 0 ${id}"}
-            Debian: {$action = "/bin/sed -i -e 's/${id}:x:/${id}:${password}:/g' /etc/shadow; chage -d 0 ${id}"}
-            default: { }
-    }
+  case $::osfamily {
+    RedHat: {$action = "/bin/sed -i -e 's/${id}:!!:/${id}:${password}:/g' /etc/shadow; chage -d 0 ${id}"}
+    Debian: {$action = "/bin/sed -i -e 's/${id}:x:/${id}:${password}:/g' /etc/shadow; chage -d 0 ${id}"}
+    default: { }
+  }
 
-    exec { $action:
-            path    => '/usr/bin:/usr/sbin:/bin',
-            onlyif  => "egrep -q  -e '${id}:!!:' -e '${id}:x:' /etc/shadow",
-            require => User[$id]
-    }
+  exec { $action:
+    path    => '/usr/bin:/usr/sbin:/bin',
+    onlyif  => "egrep -q  -e '${id}:!!:' -e '${id}:x:' /etc/shadow",
+    require => User[$id]
+  }
 }
