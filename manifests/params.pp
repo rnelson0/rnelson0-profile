3a530ae996ef54912562fc1f7b103699adcd1628
Rob Nelson <rnelson0@gmail.com>
2015-03-16 12:12:11 +0000

    Migrate sshvpn version to new params module; cleanup lint warnings in sshgw.pp

---
+++ manifests/params.pp
@@ -0,0 +1,15 @@
+# == Class: profile::params
+#
+# Parameter defaults for profile classes
+#
+# === Authors
+#
+# Rob Nelson <rnelson0@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2015 Rob Nelson
+#
+class profile::params {
+  $sshvpnversion = 'present'
+}
